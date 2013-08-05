80ebcf957a507c1ff7cdd5699b5912e23eba6acf
Bartlomiej Dworak <bdworak@hawk.iit.edu>
2013-08-05 15:16:53 -0500

    SQL schema has changed. updated schema.

--- SQL_SCHEMA/proposals.sql
+++ SQL_SCHEMA/proposals.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 03, 2013 at 12:29 AM
+-- Generation Time: Aug 05, 2013 at 10:16 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -32,7 +32,16 @@ CREATE TABLE IF NOT EXISTS `deans` (
   `school` varchar(255) NOT NULL,
   `deanEmail` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `deans`
+--
+
+INSERT INTO `deans` (`id`, `deanName`, `school`, `deanEmail`) VALUES
+(1, 'Bart Dworak', 'PHP School of ITMO', 'bdworak@hawk.iit.edu'),
+(2, 'Rima Kuprys', 'IPRO School of fun and Games', 'rima@iit.edu'),
+(3, 'Ray Trygstadt', 'School of Applied Technology', 'rkuprys@iit.edu');
 
 -- --------------------------------------------------------
 
@@ -44,7 +53,20 @@ CREATE TABLE IF NOT EXISTS `disciplines` (
   `id` int(9) NOT NULL AUTO_INCREMENT,
   `disciplineName` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `disciplines`
+--
+
+INSERT INTO `disciplines` (`id`, `disciplineName`) VALUES
+(1, 'ITM'),
+(2, 'INTM'),
+(3, 'Physics'),
+(4, 'Math'),
+(5, 'Computer Science'),
+(6, 'Computer Engineering'),
+(7, 'Electrical Engineering');
 
 -- --------------------------------------------------------
 
@@ -72,7 +94,16 @@ CREATE TABLE IF NOT EXISTS `proposals` (
   `OwnerID` int(9) NOT NULL,
   `status` int(1) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `proposals`
+--
+
+INSERT INTO `proposals` (`ID`, `Instructor`, `InstructorEmail`, `CoInstructor`, `CoInstructorEmail`, `Sponsor`, `ApprovingDean`, `Disciplines`, `Title`, `Problem`, `Objective`, `Approach`, `Semester`, `Days`, `Time`, `CourseNumber`, `OwnerID`, `status`) VALUES
+(1, 'Bart Dworak', 'bdworak@hawk.iit.edu', 'Emanual.pdf', 'emanual@iit.pdf', 'Rima K.', 1, 'a:5:{i:0;i:1;i:1;i:2;i:2;i:3;i:3;i:5;i:4;i:7;}', 'Building better communities', 'Tasks are often blah blah blah', 'Blah blah', 'Blah blah blah', 'SPRING2014', 'a:0:{}', 'Evening', 0, 1, 0),
+(2, 'Bart Dworak', 'bdworak@hawk.iit.edu', 'Emanual.pdf', 'emanual@iit.pdf', 'Rima K.', 1, 'a:5:{i:0;i:1;i:1;i:2;i:2;i:3;i:3;i:4;i:4;i:7;}', 'Why the coding cave needs more TV''s', 'lsdkmfsldkm', 'sdlfkmsfkldm', 'sldkfmsklm', 'SPRING2014', 'a:0:{}', 'Afternoon', 0, 1, 2),
+(3, 'Bart Dworak', 'bdworak@hawk.iit.edu', 'Emanual.pdf', 'emanual@iit.pdf', 'Rima K.', 1, 'a:3:{i:0;i:2;i:1;i:4;i:2;i:6;}', 'Bart is awesome, no?', 'lfksmsdkl', 'lsdkfsldkfm', 'vlskmdflkmf', 'SPRING2015', 'a:0:{}', 'Afternoon', 0, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -84,7 +115,16 @@ CREATE TABLE IF NOT EXISTS `proposals_control` (
   `proposalID` int(9) NOT NULL AUTO_INCREMENT,
   `lastRevision` int(9) NOT NULL,
   PRIMARY KEY (`proposalID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `proposals_control`
+--
+
+INSERT INTO `proposals_control` (`proposalID`, `lastRevision`) VALUES
+(1, 1),
+(2, 1),
+(3, 1);
 
 -- --------------------------------------------------------
 
