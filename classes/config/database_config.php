dd02b9ea1c572498c640c56b27154c3b1b6f7f06
administrator <administrator@IdeaProServer.iit.edu>
2013-09-13 01:11:18 -0500

    updated db config and gitignore file

--- classes/config/database_config.php
+++ classes/config/database_config.php
@@ -1,6 +1,6 @@
 <?php
 $database_location = '127.0.0.1';
 $database_username = 'proposals';
-$database_password = '123456';
+$database_password = 'yLe4UmMXQLv4WEVS';
 $database_name = 'proposals';
 ?>
